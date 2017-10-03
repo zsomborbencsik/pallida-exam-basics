2ca95ea7c5460f86fc19417764628bbcc7906220
gygabor <gyorgyg@gmail.com>
2017-10-03 13:47:36 +0200

    feat(unique_chars): add example

--- uniquechars/unique_chars.py
+++ uniquechars/unique_chars.py
@@ -1,3 +1,6 @@
 # Create a function called `unique_characters` that takes a string as parameter
 # and returns a list with the unique letters of the given string
 # Create basic unit tests for it with at least 3 different test cases
+print(unique_characters("anagram"))
+# Should print out:
+# ["n", "g", "r", "m"]
