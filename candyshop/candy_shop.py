c0198e83ed26cc5788fd128a5fba60da113d90c1
gygabor <gyorgyg@gmail.com>
2017-10-03 16:28:37 +0200

    fix(candy_shop.py) fix exercise

--- candyshop/candy_shop.py
+++ candyshop/candy_shop.py
@@ -6,14 +6,14 @@
 # we can raise their prices with a given percentage
 #
 # Create a CandyShop class
-# It can store sugar and money as income. The constructor should take the amount of sugar.
+# It can store sugar and money as income. The constructor should take the amount of sugar in gramms.
 # we can create lollipops and candies store them in the CandyShop's storage
-# If we create a candie or lollipop the CandyShop's sugar reduce the sugar with the amount of the sweets
+# If we create a candie or lollipop the CandyShop's sugar amount gets reduced by the amount needed to create the sweets
 # We can raise the prices of all candies and lollipops with a given percentage
 # We can sell candie or lollipop with a given number as amount
-# If we sell sweets the income will raise with the price of the sweets and we delete it from the inventory
+# If we sell sweets the income will be increased by the price of the sweets and we delete it from the inventory
 # We can buy sugar with a given number as amount. The price of 1000gr sugar is 100$
-# If we buy sugar we can raise the CandyShop's amount of sugar and reduce the income.
+# If we buy sugar we can raise the CandyShop's amount of sugar and reduce the income by the price of it.
 # The CandyShop should be represented as string in this format:
 # "Inventory: 3 candies, 2 lollipops, Income: 100, Sugar: 400gr"
 
