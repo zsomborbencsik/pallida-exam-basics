2df022552374c03ef45bfbec99899f2b06bcba77
aze <adam.gyulavari@lab.coop>
2017-10-05 12:25:56 +0200

    feat(java) add java exercises

---
+++ uniquechars/UniqueChars.java
@@ -0,0 +1,10 @@
+public class UniqueChars {
+    public static void main(String[] args) {
+        // Create a function called `uniqueCharacters` that takes a string as parameter
+        // and returns a list with the unique letters of the given string
+        // Create basic unit tests for it with at least 3 different test cases
+        System.out.println(uniqueCharacters("anagram"));
+        // Should print out:
+        // ["n", "g", "r", "m"]
+    }
+}
